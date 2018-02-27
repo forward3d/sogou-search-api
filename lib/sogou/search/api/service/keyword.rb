0ba3198262cb36ab1b975da3433dac4fd8f525db
Min.Kim <minsikzzang@gmail.com>
2018-02-27 13:16:35 +0000

    Add space after = operator / refactor Keyword

--- lib/sogou/search/api/service/keyword.rb
+++ lib/sogou/search/api/service/keyword.rb
@@ -9,18 +9,24 @@ module Sogou
             super('http://api.agent.sogou.com:8080/sem/sms/v1/CpcService?wsdl')
           end
 
-          def get_cpc_by_cpc_grp_id(grp_ids, valid_keyword_only=true)
-            resp = client.call(:get_cpc_by_cpc_grp_id,
-                               message: { cpc_grp_ids: grp_ids, get_temp: valid_keyword_only ? 0 : 1 })
-            puts resp.header
-            resp.body
+          def get_cpc_by_cpc_grp_id(grp_ids, valid_keyword_only = true, options = {}, &block)
+            command = make_command(:get_cpc_by_cpc_grp_id,
+                                   params: {
+                                     cpc_grp_ids: grp_ids,
+                                     get_temp: valid_keyword_only ? 0 : 1
+                                   },
+                                   options: options)
+            execute_command(command, &block)
           end
 
-          def get_cpc_id_by_cpc_grp_id(grp_ids, valid_keyword_only=true)
-            resp = client.call(:get_cpc_id_by_cpc_grp_id,
-                               message: { cpc_grp_ids: grp_ids, get_temp: valid_keyword_only ? 0 : 1  })
-            puts resp.header
-            resp.body
+          def get_cpc_id_by_cpc_grp_id(grp_ids, valid_keyword_only = true, options = {}, &block)
+            command = make_command(:get_cpc_id_by_cpc_grp_id,
+                                   params: {
+                                     cpc_grp_ids: grp_ids,
+                                     get_temp: valid_keyword_only ? 0 : 1
+                                   },
+                                   options: options)
+            execute_command(command, &block)
           end
         end
       end
--- lib/sogou/search/api/service/promotion_group.rb
+++ lib/sogou/search/api/service/promotion_group.rb
@@ -9,14 +9,14 @@ module Sogou
             super('http://api.agent.sogou.com:8080/sem/sms/v1/CpcGrpService?wsdl')
           end
 
-          def get_cpc_grp_by_cpc_plan_id(plan_ids, options={}, &block)
+          def get_cpc_grp_by_cpc_plan_id(plan_ids, options = {}, &block)
             command = make_command(:get_cpc_grp_by_cpc_plan_id,
                                    params: { cpc_plan_ids: plan_ids },
                                    options: options)
             execute_command(command, &block)
           end
 
-          def get_cpc_grp_id_by_cpc_plan_id(plan_ids, options={}, &block)
+          def get_cpc_grp_id_by_cpc_plan_id(plan_ids, options = {}, &block)
             command = make_command(:get_cpc_grp_id_by_cpc_plan_id,
                                    params: { cpc_plan_ids: plan_ids },
                                    options: options)
