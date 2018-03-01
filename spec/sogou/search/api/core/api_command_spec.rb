b13188717c5aa15afa98a2a83f29b4a3da7ab506
Min.Kim <minsikzzang@gmail.com>
2018-03-01 13:53:02 +0000

    Add some specs

--- spec/sogou/search/api/core/api_command_spec.rb
+++ spec/sogou/search/api/core/api_command_spec.rb
@@ -3,13 +3,28 @@ require 'spec_helper'
 include Sogou::Search::Api
 
 describe Core::ApiCommand do
-  describe '#execute' do
-    context 'when all ok' do
+  subject(:this) { described_class.new(operation) }
 
+  describe '#execute' do
+    subject(:execute) { this.execute(client) }
+    let(:operation) { :test }
+    let(:client) { double('client') }
+
+    context 'when all ok' do
+      context 'with block' do
+        it { execute }
+      end
+
+      context 'without block' do
+      end
     end
 
     context 'when error occurrs' do
+      context 'with block' do
+      end
 
+      context 'without block' do
+      end
     end
   end
 
