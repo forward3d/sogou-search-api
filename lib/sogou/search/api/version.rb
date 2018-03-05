c2458ce21768883f786ef3e032bf12e47860ec84
Min.Kim <minsikzzang@gmail.com>
2018-03-05 13:29:01 +0000

    Bump version to 0.0.2

--- lib/sogou/search/api/version.rb
+++ lib/sogou/search/api/version.rb
@@ -1,7 +1,7 @@
 module Sogou
   module Search
     module Api
-      VERSION = '0.0.1'
+      VERSION = '0.0.2'
 
       OS_VERSION = begin
         if RUBY_PLATFORM =~ /mswin|win32|mingw|bccwin|cygwin/
